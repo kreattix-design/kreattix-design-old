114c2f072a989745b2930dec6058073b581ba232
Prince Patel <ext_prpatel@falabella.cl>
2022-11-03 13:12:09 +0530

    helper color shades

--- example/src/App.js
+++ example/src/App.js
@@ -1,32 +1,28 @@
 import logo from './logo.svg'
 import './App.css'
-import { Icon, Colors } from 'kreattix-design'
+import { Box } from 'kreattix-design'
 
 const App = () => {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-        <Icon
-          icon='Logo'
-          shape='square'
-          type='toned'
-          size='large'
-          color={Colors.purple200}
-        />
-      </header>
-    </div>
+    <Box
+      className='App-header bg-dark'
+      size='fullscreen'
+      align='center'
+      justify='center'
+    >
+      <img src={logo} className='App-logo' alt='logo' />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className='App-link'
+        href='https://reactjs.org'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        Learn React
+      </a>
+    </Box>
   )
 }
 
